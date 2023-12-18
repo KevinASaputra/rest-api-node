98541b268b54d1b093001258edf8c36efd46b20e
Kevin <kevinadikasaputragithub@gmail.com>
2023-12-18 20:54:16 +0700

    :hammer: Chore: update route api

---
+++ routes/ProductRoute.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const routes = express.Router();
+
+const {
+  getProducts,
+  updateProducts,
+  deleteProducts,
+  getSingleProducts,
+  createProducts,
+} = require("../controllers/productsControllers");
+
+// get all products
+routes.get("/", getProducts);
+
+// update product
+routes.put("/:id", updateProducts);
+
+// delete product
+routes.delete("/:id", deleteProducts);
+
+// get single product
+routes.get("/:id", getSingleProducts);
+
+// create product
+routes.post("/create", createProducts);
+
+module.exports = routes;
